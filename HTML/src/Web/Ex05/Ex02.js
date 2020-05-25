b5306443a29895eccb0eb8ffd348f9f29e220124
Swiftly <ivecis@live.co.kr>
2020-05-25 16:22:40 +0900

    10 This is javaScript

--- HTML/src/Web/Ex05/Ex02.js
+++ HTML/src/Web/Ex05/Ex02.js
@@ -2,15 +2,18 @@ function Calculate() {
     let list = document.getElementsByTagName('input');
     document.getElementById('result').value = 0;
     for (let i = 0; i < list.length; i++) {
-        list[i].addEventListener('click', activateList);
+        list[i].addEventListener('click', buttonAction);
     }
 }
 
-function activateList(event) {
+function buttonAction() {
+    let result = document.getElementById('result');
     if (this.value === 'c')
-        document.getElementById('result').value = 0;
-    else if (document.getElementById('result').value === '0')
-        document.getElementById('result').value = this.value;
+        result.value = 0;
+    else if (result.value === '0')
+        result.value = this.value;
+    else if (this.value === '<')
+        result.value = result.value.substr(0, result.value.length - 1);
     else
-        document.getElementById('result').value += this.value;
+        result.value += this.value;
 }
