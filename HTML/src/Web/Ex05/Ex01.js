67d075ce178194fe1ef45df38f946fb8f26247c3
Swiftly <ivecis@live.co.kr>
2020-05-25 12:54:36 +0900

    8 This is javaScript

--- HTML/src/Web/Ex05/Ex01.js
+++ HTML/src/Web/Ex05/Ex01.js
@@ -1,26 +1,25 @@
 function ListClick() {
     let list = document.getElementById('list');
-    let liList = list.getElementsByTagName('li');
-    for (let i = 0; i < liList.length; i++)
-        liList[i].addEventListener('click', activateList);
+    list.addEventListener('click', activateList);
 }
 
 function activateList(event) {
     let title = document.getElementById('title');
     let liList = event.target.parentElement.children;
-
-    title.innerText = this.innerText;
     for (let i = 0; i < liList.length; i++)
         liList[i].removeAttribute('class')
-    this.setAttribute('class', 'active');
+
+    title.innerText = event.target.innerText;
+    event.target.setAttribute('class', 'active');
 }
 
 function buttonClick() {
     let addButton = document.getElementById('addButton');
-    addButton.addEventListener('click', event => {
+    addButton.addEventListener('click', () => {
         let inputText = prompt('추가할 내용을 입력하세요.');
-
         let list = document.getElementById('list');
-        list.innerHTML += '<li>' + inputText + '</li>';
+        let li = document.createElement('li');
+        li.innerText = inputText;
+        list.appendChild(li);
     });
 }
