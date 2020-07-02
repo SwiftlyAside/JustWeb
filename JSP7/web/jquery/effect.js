a957682ca01f646426159f71acc7fd188b6d6507
Swiftly <ivecis@live.co.kr>
2020-07-02 11:02:44 +0900

    112 The Web

--- JSP7/web/jquery/effect.js
+++ JSP7/web/jquery/effect.js
@@ -4,13 +4,35 @@ $(document).ready(() => {
     width: '300px',
   });
 
+  function al(alarm) {
+    alert(alarm);
+  }
+
   $('#btnHide').click(() => {
-    $('.ani1').hide(1000, () => alert('사라짐'));
+    $('.ani1').hide(2000, () => al('this is alarm'));
   });
   $('#btnShow').click(() => {
-    $('.ani1').show(1000);
+    $('.ani1').show(2000);
   });
   $('#btnToggle').click(() => {
-    $('.ani1').toggle('fast');
+    $('.ani1').toggle(3000);
+  });
+  $('#btnSlideUp').click(() => {
+    $('.ani2').slideUp(3000);
+  });
+  $('#btnSlideDown').click(() => {
+    $('.ani2').slideDown(3000);
+  });
+  $('#btnSlideToggle').click(() => {
+    $('.ani2').slideToggle(3000);
+  });
+  $('#btnFadeIn').click(() => {
+    $('.ani3').fadeIn(3000);
+  });
+  $('#btnFadeOut').click(() => {
+    $('.ani3').fadeOut(3000);
+  });
+  $('#btnFadeTo').click(() => {
+    $('.ani3').fadeTo(3000, 0.5);
   });
 });
